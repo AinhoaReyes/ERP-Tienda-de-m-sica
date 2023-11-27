04e0f9ec50c37d204a46d6369f532109a085f792
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2023-11-27 17:48:13 +0100

    feat(core): added routes object

---
+++ src/main/resources/static/js/erp/routes.js
@@ -0,0 +1,22 @@
+const _routes = [
+  {
+    name: "Home",
+    href: "/",
+    icon: homeIcon,
+  },
+  {
+    name: "Accounting",
+    href: "/accounting",
+    icon: accountingIcon,
+  },
+  {
+    name: "Warehouses",
+    href: "/warehouses",
+    icon: warehouseIcon,
+  },
+  {
+    name: "Products",
+    href: "/products",
+    icon: productIcon,
+  },
+];
