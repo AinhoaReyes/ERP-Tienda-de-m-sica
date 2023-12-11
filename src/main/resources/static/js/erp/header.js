2453156abc6e669c3f9ef322daf2a5dc0908ec5d
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2023-12-11 17:18:38 +0100

    feat: added isActive to routes in searchbar

--- src/main/resources/static/js/erp/header.js
+++ src/main/resources/static/js/erp/header.js
@@ -1,7 +1,19 @@
+const setSearchRoutes = (array) =>
+  (document.getElementById("route-search-routes").innerHTML = array
+    .map((route) => SearchRoute(route.name, route.href, route.icon))
+    .join(""));
+
+const isActive = (href) => {
+  if (window.location.pathname === "/" && href === "/") {
+    return true;
+  }
+  return href !== "/" && window.location.pathname.includes(href);
+};
+
 const SearchRoute = (name, href, icon) =>
   `<li class="row-item">
     <a class="link-wrapper" href="${href}" title="Navigate to ${name}">
-      <div class="icon-wrapper">${icon}</div>
+      <div class="icon-wrapper ${isActive(href) ? "active" : ""}">${icon}</div>
       <p>${name}</p>
     </a>
   </li>`;
@@ -12,10 +24,24 @@ document.getElementById("modal-close-icon").innerHTML = crossIcon;
 document
   .getElementById("header-modal-content")
   .addEventListener("click", (e) => e.stopPropagation());
-const headerModalRef = document.getElementById("header-modal");
 
+const headerModalRef = document.getElementById("header-modal");
 const handleSearchbarClick = () => setIsSearchOpen(true);
-const handleModalClose = () => setIsSearchOpen(false);
+const handleModalClose = () => {
+  routeInputRef.value = "";
+  setSearchRoutes(_routes);
+  setIsSearchOpen(false);
+};
+
+const routeInputRef = document.getElementById("route-search-input");
+routeInputRef.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  setSearchRoutes(
+    (filteredRoutes = _routes.filter((i) =>
+      i.name.toLowerCase().startsWith(value.toLowerCase())
+    ))
+  );
+});
 
 const setIsSearchOpen = (value) => {
   headerModalRef.hidden = !value;
@@ -39,9 +65,4 @@ const activeRoute = _routes.find((i) => {
 });
 
 document.getElementById("header-route-icon").innerHTML = activeRoute.icon;
-
-const filteredRoutes = _routes.filter((i) => i != activeRoute);
-
-document.getElementById("route-search-routes").innerHTML = filteredRoutes
-  .map((route) => SearchRoute(route.name, route.href, route.icon))
-  .join("");
+setSearchRoutes(_routes);
