d7663cd94cc739095791074246c6774bd0d2b70d
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2023-12-14 21:16:22 +0100

    feat(core): added WarehouseDAO

---
+++ src/main/java/grupoB/erp/dao/WarehouseDAO.java
@@ -0,0 +1,7 @@
+package grupoB.erp.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import grupoB.erp.domain.Warehouse;
+
+public interface WarehouseDAO extends CrudRepository<Warehouse, Long> {
+}
