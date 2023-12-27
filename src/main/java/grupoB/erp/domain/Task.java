8631643d93804cfd19336f6c0e65c8e77762f818
Ainhoa <reyes.moreno.ainhoa.scarlet@alumnat.copernic.cat>
2023-12-27 18:05:00 +0100

    New entitiees

---
+++ src/main/java/grupoB/erp/domain/Task.java
@@ -0,0 +1,51 @@
+package grupoB.erp.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "task")
+public class Task implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    enum TaskPriority {
+        highest, high, medium, low, lowest;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskPriority priority;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "all_day", columnDefinition = "boolean default true")
+    private boolean allDay;
+
+    @OneToMany(mappedBy = "task")
+    private Set<Calendar> calendars;
+
+
+}
