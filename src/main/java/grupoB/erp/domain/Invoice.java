79e9712031e7bdb5c7c8ed85169dcef0bd7a8437
Victor Tamayo Viera <106868505+freckledAsf@users.noreply.github.com>
2024-01-05 20:56:52 +0100

    chore: update Invoice class

--- src/main/java/grupoB/erp/domain/Invoice.java
+++ src/main/java/grupoB/erp/domain/Invoice.java
@@ -23,7 +23,6 @@ public class Invoice implements Serializable {
     private String ref;
 
     @OneToOne
-    @MapsId
     @JoinColumn(name = "ref")
     private Order order;
 
@@ -36,18 +35,9 @@ public class Invoice implements Serializable {
     @Column(nullable = false)
     private double total;
 
-    @Column(name = "account_number", nullable = false)
-    private String accountNumber;
-
     @CreationTimestamp
     @Column(name = "created_at")
     private Timestamp createdAt;
-
-    public Invoice(double amount, Timestamp createdAt, Order order) {
-        this.amount = amount;
-        this.createdAt = createdAt;
-        this.order = order;
-    }
 
     public Invoice() {
     }
